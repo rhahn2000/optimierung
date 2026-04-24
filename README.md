ea5e32362fce65d223a835f6418ace2561f1b959
Becca <128615717+rhahn2000@users.noreply.github.com>
2026-04-24 11:48:39 +0200

    added math and geometry libs

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Bericht
+
+## 24/04/26
+- Einbinden der Geometry und Math Bibliotheken
+- Erstes Beispiel ausgetestet
+
 ## 24/03/26
 - Setup der Umgebung in Visual Studio Code mit Gemini CLI
 - erste kleinere Experimente
--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include "math/math.h"
+#include "geometry/geometry.h"
 
 int main() {
     std::cout << "Hello World" << std::endl;
+    Vector3df v1 = {1.0f, 0.0f, 0.0f};
+    Vector3df v2 = {0.0f, 1.0f, 0.0f};
+    // Vector3df v3 = v1.cross_product(v2);
+    // std::cout << v3[0] << " " << v3[1] << " " << v3[2] << std::endl;
+    Vector3df v3 = {0.0f, 0.0f, 1.0f};
+    Triangle3df triangle(v1, v2, v3);
+    Ray3df ray = { 
+        {0.2f, 1.0f, 0.2f}, // origin
+        {0.0f, -1.0f, 0.0f}  // direction
+    };
+    Intersection_Context<float, 3u> context;
+    
+    if (triangle.intersects(ray, context)) {
+        std::cout << "Treffer!" << std::endl;
+        std::cout << "Abstand t: " << context.t << std::endl;
+        std::cout << "Schnittpunkt: (" << context.intersection[0] << ", " 
+                  << context.intersection[1] << ", " << context.intersection[2] << ")" << std::endl;
+        std::cout << "Baryzentrische Koord: u=" << context.u << ", v=" << context.v << std::endl;
+    } else {
+        std::cout << "Kein Treffer." << std::endl;
+    }
+    return 0;
 }
